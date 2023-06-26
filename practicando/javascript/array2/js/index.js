861193335557c497e2ddcddb0fc13a402b390ca4
Pato <patobsirena@gmail.com>
2023-06-26 12:38:11 -0300

    Cuarto commit

---
+++ practicando/javascript/array2/js/index.js
@@ -0,0 +1,29 @@
+let alumnos = ['Tutty','Agustin','Nati','Christian','Jaqui'];
+
+
+//  Para obtener la posicion de un elemento del array
+console.log(alumnos.indexOf('Nati'));
+
+//  Eliminar uno o varios elementos
+
+// alumnos.splice(0,2);
+// console.log(alumnos);
+
+//  Para buscar un elemento en el array
+
+console.log(alumnos.includes('Jaqui'));
+
+//  Para ordenar el array alfabeticamente
+
+console.log(alumnos.sort());
+
+//  Para ordenar el array alfabeticamente al reves
+
+console.log(alumnos.reverse());
+
+// Para concatenar arrays
+
+let alumnos2 = ['Pato','Mariano','Joaquin'];
+let comision = alumnos.concat(alumnos2);
+
+
