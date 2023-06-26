cd57d0476c25dab051d1c8bec6e28768a94c2ba1
Pato <patobsirena@gmail.com>
2023-06-26 08:52:01 -0300

    Segundo commit

---
+++ practicando/javascript/condicionales/js/index.js
@@ -0,0 +1,13 @@
+let num1 = parseInt(prompt("Ingresa un numero"));
+let num2 = parseInt(prompt("Ingresa otro numero"));
+let suma = num1 + num2;
+
+console.log(suma);
+
+if (suma % 2 === 0) {
+    console.log("Numero par");
+} else{
+    console.error("El numero no es par");
+}
+
+
